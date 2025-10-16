6678484801555c628d02ddd636a14ba7711d1866
csh <458761603@qq.com>
2025-10-16 21:40:13 +0800

    Optimize package.sh

--- package.sh
+++ package.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 mkdir package
+echo "build echokit(boards)"
+
 cargo build --release
 cp target/xtensa-esp32s3-espidf/release/echokit package/echokit_boards
 espflash save-image --chip esp32s3 --merge --flash-size 16mb target/xtensa-esp32s3-espidf/release/echokit package/echokit_boards.bin
 
+echo "build echokit(cube)"
 cargo build --release --features=cube
 cp target/xtensa-esp32s3-espidf/release/echokit package/echokit_cube
 espflash save-image --chip esp32s3 --merge --flash-size 16mb target/xtensa-esp32s3-espidf/release/echokit package/echokit_cube.bin
