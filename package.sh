bd0c6d9682e9a7a2662e0f108f1cd56695f43d7a
csh <458761603@qq.com>
2025-11-07 19:08:39 +0800

    CI: fix package

--- package.sh
+++ package.sh
@@ -5,21 +5,21 @@ build_boards(){
     echo "build echokit(boards/DIY)"
     cargo build --release
     cp target/xtensa-esp32s3-espidf/release/echokit package/echokit_boards
-    espflash save-image --chip esp32s3 --merge --flash-size 16mb target/xtensa-esp32s3-espidf/release/echokit package/echokit_boards.bin
+    espflash save-image --chip esp32s3 --merge --flash-size 16mb --partition-table partitions.csv target/xtensa-esp32s3-espidf/release/echokit package/echokit_boards.bin
 }
 
 build_cube2(){
     echo "build echokit(cube2)"
     cargo build --release --features=cube2
     cp target/xtensa-esp32s3-espidf/release/echokit package/echokit_cube2
-    espflash save-image --chip esp32s3 --merge --flash-size 16mb target/xtensa-esp32s3-espidf/release/echokit package/echokit_cube2.bin
+    espflash save-image --chip esp32s3 --merge --flash-size 16mb --partition-table partitions.csv target/xtensa-esp32s3-espidf/release/echokit package/echokit_cube2.bin
 }
 
 build_box(){
     echo "build echokit(box)"
     cargo build --release --no-default-features --features=box
     cp target/xtensa-esp32s3-espidf/release/echokit package/echokit_box
-    espflash save-image --chip esp32s3 --merge --flash-size 16mb target/xtensa-esp32s3-espidf/release/echokit package/echokit_box.bin
+    espflash save-image --chip esp32s3 --merge --flash-size 16mb --partition-table partitions.csv target/xtensa-esp32s3-espidf/release/echokit package/echokit_box.bin
 }
 
 # 如果没有参数，默认构建全部
