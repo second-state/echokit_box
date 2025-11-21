e27381c7697d37d5155d1069cef3417f0c154fac
csh <458761603@qq.com>
2025-11-22 03:51:03 +0800

    fix(ci): fmt

--- src/boards/atom_box.rs
+++ src/boards/atom_box.rs
@@ -144,7 +144,8 @@ pub fn lcd_init(
 
 pub fn flush_display(color_data: &[u8], x_start: i32, y_start: i32, x_end: i32, y_end: i32) -> i32 {
     debug_assert_eq!(
-        x_end - x_start, DISPLAY_WIDTH as i32,
+        x_end - x_start,
+        DISPLAY_WIDTH as i32,
         "x_end - x_start must be equal to DISPLAY_WIDTH"
     );
     unsafe {
